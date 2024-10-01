3affdb4865a1bfec1caedde38821bc4d6819e854
Dante4K18 <dan@dan>
2024-10-01 23:26:35 +0300

    completed-attributes-and-properties

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    approved_jobs = [
+        "Admin", "Customer Service", "Human Resources", "ITC", "Production",
+        "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"
+    ]
+
+    def _init_(self, name:str = "Unknown", job: str = "Unknown"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self.name
+    
+    @name.setter
+    def name(self, value:str):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self.name = value.title()
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value:str):
+        if value in self.approved_jobs:
+            self.job = value
+        else:
+            print("Job must be in list of approved jobs.")
